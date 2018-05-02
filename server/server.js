74b5bb9d448b0529b232ec809144af4bca2f1072
jsc215 <jeffcomertennis@gmail.com>
2018-05-02 07:12:26 -0400

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 
-const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,43 +17,49 @@ app.post('/todos', (req, res) => {
   let todo = new Todo({
     text: req.body.text
   });
-  todo.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
-    res.status(400).send(e)
-  });
+  todo.save().then(
+    doc => {
+      res.send(doc);
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
 });
 
 // GET all todos
 app.get('/todos', (req, res) => {
-  Todo.find().then(todos => {
-    res.send({todos})
-  }, (e) => {
-    res.status(400).send(e);
-  });
+  Todo.find().then(
+    todos => {
+      res.send({ todos });
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
 });
 
 // GET post by id
 app.get('/todos/:id', (req, res) => {
   let id = req.params.id;
 
-   if (!ObjectID.isValid(id)) {
-      return res.status(404).send();
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
   }
-  
+
   Todo.findById(id)
-    .then((todo) => {
+    .then(todo => {
       if (!todo) {
         return res.status(404).send();
       }
-      res.send({todo});
+      res.send({ todo });
     })
-    .catch((e) => {
+    .catch(e => {
       res.status(400).send();
     });
-  });
+});
 
-  // DELETE todo
+// DELETE todo
 app.delete('/todos/:id', (req, res) => {
   let id = req.params.id;
   if (!ObjectID.isValid(id)) {
@@ -61,50 +67,50 @@ app.delete('/todos/:id', (req, res) => {
   }
 
   Todo.findByIdAndRemove(id)
-    .then((todo) => {
-      if(!todo) {
+    .then(todo => {
+      if (!todo) {
         return res.status(404).send();
       }
-      res.send({todo})
+      res.send({ todo });
     })
-    .catch((e) => {
+    .catch(e => {
       res.status(400).send();
     });
-  });
+});
 
-  // UPDATE
+// UPDATE
 
-  app.patch('/todos/:id', (req, res) =>{
-    let id = req.params.id;
-    
-    // let body = _.pick(req.body, ['text', 'completed']); 
-    // using destructuring instead of lodash method above
-    let {text, completed} = req.body
-    let body = {text, completed}
-    if (!ObjectID.isValid(id)) {
-      return res.status(404).send();
-    }
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
 
-    if (_.isBoolean(body.completed) && body.completed) {
-      body.completedAt = new Date().getTime();
-    } else {
-      body.completed = false;
-      body.completedAt = null;
-    }
-    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then(todo => {
+  // let body = _.pick(req.body, ['text', 'completed']);
+  // using destructuring instead of lodash method above
+  let { text, completed } = req.body;
+  let body = { text, completed };
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
       if (!todo) {
-        return res.status(404).send()
+        return res.status(404).send();
       }
-      res.send({todo});
-    }).catch(e => {
-      res.status(400).send
+      res.send({ todo });
     })
-  })
+    .catch(e => {
+      res.status(400).send;
+    });
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
 
-module.exports = {app};
-
-
+module.exports = { app };
