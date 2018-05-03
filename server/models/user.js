c677c99d3972fa05d735d65fec3fdaa018a256ac
jsc215 <jeffcomertennis@gmail.com>
2018-05-02 23:38:57 -0400

    add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -58,6 +58,16 @@ UserSchema.methods.generateAuthToken = function () {
     });
   };
 
+  UserSchema.methods.removeToken = function (token) {
+    let user = this;
+
+    return user.update({
+      $pull: {
+        tokens: {token}
+      }
+    });
+  }
+
 UserSchema.statics.findByToken = function (token) {
   let User = this;
   let decoded;
@@ -106,8 +116,7 @@ UserSchema.pre('save', function (next){
     });
   } else {
    next()
-
- }
+  }
 });
 
 let User = mongoose.model('User', UserSchema);
